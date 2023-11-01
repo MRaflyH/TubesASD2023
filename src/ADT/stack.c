c1d54f6309e5ab49780d0a7fe97fc6e1f975fb8e
Dzulfaqor Ali Dipangegara <whysoserius30@gmail.com>
2023-11-02 05:58:37 +0700

    stack done

---
+++ src/ADT/stack.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include "boolean.h"
+#include "stack.h"
+
+/* *** Konstruktor/Kreator *** */
+void CreateEmpty (Stack *S)
+/* I.S. sembarang; */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* jadi indeksnya antara 0.. MaxEl */
+/* Ciri stack kosong : TOP bernilai Nil */
+{
+    Top(*S) = Nil;
+}
+
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+boolean IsEmpty (Stack S)
+/* Mengirim true jika Stack kosong: lihat definisi di atas */
+{
+    return (Top(S)==Nil);
+}
+
+boolean IsFull (Stack S)
+/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+{
+    return (Top(S)==MaxEl-1);
+}
+
+/* ************ Menambahkan sebuah elemen ke Stack ************ */
+void Push (Stack * S, infotype X)
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
+/* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
+{
+    Top(*S) += 1;
+    InfoTop(*S) = X;
+}
+
+/* ************ Menghapus sebuah elemen Stack ************ */
+void Pop (Stack * S, infotype* X)
+/* Menghapus X dari Stack S. */
+/* I.S. S  tidak mungkin kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
+{
+    *X = InfoTop(*S);
+    Top(*S) -= 1;
+}
+
+void DisplayStackS(Stack s){
+   if(IsEmpty(s)){
+       printf("[]");
+   } else{
+       printf("[");
+       for(address i=0; i<Top(s); i++){
+           printf("%d,", s.T[i]);
+       }
+       printf("%d]", s.T[s.TOP]);
+   }
+}
+/* Proses : Menuliskan isi Queue dengan traversal, Queue ditulis di antara kurung 
+   siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan 
+   karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
+/* I.S. q boleh kosong */
+/* F.S. Jika q tidak kosong: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika Queue kosong : menulis [] */
--- src/ADT/stack.h
+++ src/ADT/stack.h
@@ -1,37 +1,57 @@
-#ifndef STACK_H
-#define STACK_H
+/* File : stack.h */
+/* deklarasi stack yang diimplementasi dengan tabel kontigu dan ukuran sama */
+/* TOP adalah alamat elemen puncak */
+/* Implementasi dalam bahasa C dengan alokasi statik */
+#ifndef stackt_H
+#define stackt_H
 
 #include "boolean.h"
 
-#define MaxCapacity 100
-#define IdxUndef -1
+#define Nil -1
+#define MaxEl 100
+/* Nil adalah stack dengan elemen kosong . */
 
-typedef int ElType;
-typedef int IdxType;
+typedef int infotype;
+typedef int address;   /* indeks tabel */
 
-typedef struct {
-    ElType Content[MaxCapacity];
-    IdxType IdxTop;
+/* Contoh deklarasi variabel bertype stack dengan ciri TOP : */
+/* Versi I : dengan menyimpan tabel dan alamat top secara eksplisit*/
+typedef struct { 
+  infotype T[MaxEl]; /* tabel penyimpan elemen */
+  address TOP;  /* alamat TOP: elemen puncak */
 } Stack;
+/* Definisi stack S kosong : S.TOP = Nil */
+/* Elemen yang dipakai menyimpan nilai Stack T[0]..T[MaxEl-1] */
+/* Jika S adalah Stack maka akses elemen : */
+   /* S.T[(S.TOP)] untuk mengakses elemen TOP */
+   /* S.TOP adalah alamat elemen TOP */
 
-void CreateS(Stack *s);
+/* Definisi akses dengan Selektor : Set dan Get */
+#define Top(S) (S).TOP
+#define InfoTop(S) (S).T[(S).TOP]
+
+/* ************ Prototype ************ */
+/* *** Konstruktor/Kreator *** */
+void CreateEmpty(Stack *S);
 /* I.S. sembarang; */
 /* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
 /* jadi indeksnya antara 0..MaxEl-1 (inklusif) */
 /* Ciri stack kosong : TOP bernilai Nil */
 
 /* ************ Predikat Untuk test keadaan KOLEKSI ************ */
-boolean IsEmptyS(Stack s);
+boolean IsEmpty(Stack S);
 /* Mengirim true jika Stack kosong: lihat definisi di atas */
+boolean IsFull(Stack S);
+/* Mengirim true jika tabel penampung nilai elemen stack penuh */
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
-void PushS(Stack *s, ElType x);
+void Push(Stack * S, infotype X);
 /* Menambahkan X sebagai elemen Stack S. */
 /* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
 /* F.S. TOP bertambah 1, X menjadi TOP yang baru, */
 
 /* ************ Menghapus sebuah elemen Stack ************ */
-void PopS(Stack *s, ElType *x);
+void Pop(Stack * S, infotype* X);
 /* Menghapus X dari Stack S. */
 /* I.S. S  tidak mungkin kosong */
 /* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
@@ -44,5 +64,4 @@ void DisplayStackS(Stack s);
 /* F.S. Jika q tidak kosong: [e1,e2,...,en] */
 /* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
 /* Jika Queue kosong : menulis [] */
-
 #endif
