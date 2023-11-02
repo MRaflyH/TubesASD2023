4b125ff398e14fd6567e3405a3edfdd22fb747ed
Farisiii <habibfaris141@gmail.com>
2023-11-02 08:30:57 +0700

    update set.c

---
+++ src/ADT/set.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "set.h"
+
+void CreateSet(Set *s){
+    (*s).Length = 0;
+}
+
+boolean IsEmptySet(Set *s){
+    return (*s).Length == 0;
+}
+
+void AddSet(Set *s, ElType x){
+    int i;
+    boolean found = false;
+    for (i = 0; i < (*s).Length; i++){
+        if ((*s).Content[i] == x){
+            found = true;
+            break;
+        }
+    }
+    if (!found){
+        (*s).Content[i] = x;
+        (*s).Length++;
+    }
+}
+
+void DeleteSet(Set *s, ElType x){
+    int i;
+    boolean found = false;
+    for (i = 0; i < (*s).Length; i++){
+        if ((*s).Content[i] == x){
+            found = true;
+            break;
+        }
+    }
+    if (found){
+        for (int j = i; j < (*s).Length - 1; j++){
+            (*s).Content[j] = (*s).Content[j+1];
+        }
+        (*s).Length--;
+    }
+}
+
+void GetSet(Set s, int i){
+    if (i < (s).Length){
+        printf("%d\n", (s).Content[i]);
+    }
+}
+
+void DisplaySet(Set s){
+    for (int i = 0; i < s.Length; i++){
+        printf("%d\n", s.Content[i]);
+    }
+}
