e35a98baaad9be7a748fb6b0c2874212a133e97b
MRaflyH <raflyhendrakusuma@gmail.com>
2023-11-08 17:26:19 +0700

    Create console.c

---
+++ src/console.c
@@ -0,0 +1,24 @@
+#include "console.h"
+
+void START();
+void LOAD();
+void LISTDEFAULT();
+void LISTPLAYLIST();
+void PLAYSONG();
+void PLAYPLAYLIST();
+void QUEUESONG();
+void QUEUEPLAYLIST();
+void QUEUESWAP();
+void QUEUEREMOVE();
+void QUEUECLEAR();
+void SONGNEXT();
+void SONGPREVIOUS();
+void PLAYLISTCREATE();
+void PLAYLISTADD();
+void PLAYLISTSWAP();
+void PLAYLISTREMOVE();
+void PLAYLISTDELETE();
+void STATUS();
+void SAVE();
+void QUIT();
+void HELP();
