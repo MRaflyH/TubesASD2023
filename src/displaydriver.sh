438fef3e4348b63a9949bb079b29ddce6a3aeaaf
MRaflyH <raflyhendrakusuma@gmail.com>
2023-11-08 07:15:49 +0700

    Update displaydriver.sh

--- src/displaydriver.sh
+++ src/displaydriver.sh
@@ -1,5 +1,5 @@
 # YANG DIGANTI CUMAN "display.c" "displaydriver.c" JADI "FILENAME.c" "FILENAMEDRIVER.c"
-gcc "ADT/kata.c" "ADT/mesinkarakter.c" "ADT/mesininput.c" "ADT/listdinamik.c" "ADT/liststatik.c" "ADT/map.c" "ADT/queue.c" "ADT/set.c" "ADT/stack.c" "ADT/strukturberkait.c" "data.c" "display.c" "displaydriver.c" -o main
+gcc "ADT/kata.c" "ADT/detaillagu.c" "ADT/mesinkarakter.c" "ADT/mesininput.c" "ADT/listdinamik.c" "ADT/liststatik.c" "ADT/map.c" "ADT/queue.c" "ADT/set.c" "ADT/stack.c" "ADT/strukturberkait.c" "data.c" "display.c" "displaydriver.c" -o main
 ./main
 
 # CARA NGERUN UNTUK LINUX (CARA WINDOWS LAGI AKU CARI)
