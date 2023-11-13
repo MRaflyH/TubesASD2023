a6510275043bada0fa0cb5b9e366ebd22398e59a
MRaflyH <raflyhendrakusuma@gmail.com>
2023-11-13 17:04:35 +0700

    Update mesinkarakter.h

--- src/ADT/mesinkarakter.h
+++ src/ADT/mesinkarakter.h
@@ -11,9 +11,18 @@ extern char CurrentChar;
 extern boolean EOP;
 
 void StartRead(char * FileName);
-
+/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
+   Karakter pertama yang ada pada pita posisinya adalah pada jendela.
+   Pita baca diambil dari FileName.
+   I.S. : sembarang
+   F.S. : currentChar adalah karakter pertama pada pita
+          Jika currentChar != EOF maka EOP akan padam (false)
+          Jika currentChar = EOF maka EOP akan menyala (true) */
 
 void StartWrite(char * FileName);
+/* Mesin siap dioperasikan. Pita disiapkan untuk ditulis.
+   I.S. : sembarang
+   F.S. : FileName dalam state dibuka dan siap ditulis */
 
 void Start();
 /* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
@@ -32,19 +41,34 @@ void Adv();
           Jika  currentChar = MARK maka EOP akan menyala (true) */
 
 void AdvWrite(char cc);
+/* Pita dimajukan satu karakter.
+   I.S. : sembarang
+   F.S. : character terakhir pada pita adalah cc */
 
 void AdvWriteBlank();
+/* Pita dimajukan satu karakter.
+   I.S. : sembarang
+   F.S. : character terakhir pada pita adalah Blank */
 
 void AdvWriteMark();
+/* Pita dimajukan satu karakter.
+   I.S. : sembarang
+   F.S. : character terakhir pada pita adalah Mark */
 
 void AdvWriteNewLine();
+/* Pita dimajukan satu karakter.
+   I.S. : sembarang
+   F.S. : character terakhir pada pita adalah NewLine */
 
 void EndWrite();
+/* Menutup pita.
+   I.S. : pita terbuka
+   F.S. : pita tertutup */
 
 char GetCC();
 /* Mengirimkan currentChar */
 
 boolean IsEOP();
-/* Mengirimkan true jika currentChar = MARK */
+/* Mengirimkan true jika feof(pita) == true */
 
 #endif
