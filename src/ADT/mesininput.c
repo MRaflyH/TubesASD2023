9bf5411d6d86b1411f6a407897adab1bde2d2805
MRaflyH <raflyhendrakusuma@gmail.com>
2023-11-13 16:08:28 +0700

    Update mesininput.c

--- src/ADT/mesininput.c
+++ src/ADT/mesininput.c
@@ -1,91 +1,91 @@
 #include "mesininput.h"
 
 void IgnoreBlanksI() {
-   while (CurrentChar == Blank || CurrentChar == NewLine) {
-      Adv();
-   }
+    while (CurrentChar == Blank || CurrentChar == NewLine) {
+        Adv();
+    }
 }
 
 void StartWordI() {
-   Start();
-   IgnoreBlanksI();
-   if (CurrentChar == Mark) {
-      EndWord = true;
-   }
-   else {
-      EndWord = false;
-      CopyWordI();
-   }
-   IgnoreBlanksI();
+    Start();
+    IgnoreBlanksI();
+    if (CurrentChar == Mark) {
+        EndWord = true;
+    }
+    else {
+        EndWord = false;
+        CopyWordI();
+    }
+    IgnoreBlanksI();
 }
 void StartLineI() {
-   Start();
-   IgnoreBlanksI();
-   if (CurrentChar == Mark) {
-      EndWord = true;
-   }
-   else {
-      EndWord = false;
-      CopyLineI();
-   }
-   IgnoreBlanksI();
+    Start();
+    IgnoreBlanksI();
+    if (CurrentChar == Mark) {
+        EndWord = true;
+    }
+    else {
+        EndWord = false;
+        CopyLineI();
+    }
+    IgnoreBlanksI();
 }
 
 void AdvWordI() {
-   IgnoreBlanksI();
-   if (CurrentChar == Mark) {
-      EndWord = true; 
-   }
-   else {
-      CopyWordI();
-   }
-   IgnoreBlanksI();
+    IgnoreBlanksI();
+    if (CurrentChar == Mark) {
+        EndWord = true; 
+    }
+    else {
+        CopyWordI();
+    }
+    IgnoreBlanksI();
 }
 void AdvLineI() {
-   IgnoreBlanksI();
-   if (CurrentChar == Mark) {
-      EndWord = true; 
-   }
-   else {
-      CopyLineI();
-   }
-   IgnoreBlanksI();
+    IgnoreBlanksI();
+    if (CurrentChar == Mark) {
+        EndWord = true; 
+    }
+    else {
+        CopyLineI();
+    }
+    IgnoreBlanksI();
 }
 
 void CopyWordI() {
-   int i = 0;
-   while (CurrentChar != Mark && CurrentChar != Blank && CurrentChar != NewLine) {
-      CurrentWord.Content[i] = CurrentChar;
-      Adv();
-      i += 1;
-   }
+    int i = 0;
+    while (CurrentChar != Mark && CurrentChar != Blank && CurrentChar != NewLine) {
+        CurrentWord.Content[i] = CurrentChar;
+        Adv();
+        i += 1;
+    }
 
-   if (i < MaxChar) {
-      CurrentWord.Length = i;
-   }
-   else {
-      CurrentWord.Length = MaxChar;
-   }
+    if (i < MaxChar) {
+        CurrentWord.Length = i;
+    }
+    else {
+        CurrentWord.Length = MaxChar;
+    }
 } 
 void CopyLineI() {
-   int i = 0;
-   while (CurrentChar != Mark) {
-      CurrentWord.Content[i] = CurrentChar;
-      Adv();
-      i += 1;
-   }
+    int i = 0;
+    while (CurrentChar != Mark) {
+        CurrentWord.Content[i] = CurrentChar;
+        Adv();
+        i += 1;
+    }
 
-   if (i < MaxChar) {
-      CurrentWord.Length = i;
-   }
-   else {
-      CurrentWord.Length = MaxChar;
-   }
+    if (i < MaxChar) {
+        CurrentWord.Length = i;
+    }
+    else {
+        CurrentWord.Length = MaxChar;
+    }
 
-   i = CurrentWord.Length - 1;
+    i = CurrentWord.Length - 1;
 
-   while (CurrentWord.Content[i] == Blank) {
-      i -= 1;
-      CurrentWord.Length -= 1;
-   }
+    while (CurrentWord.Content[i] == Blank) {
+        i -= 1;
+        CurrentWord.Length -= 1;
+    }
 } 
