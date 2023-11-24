10f9d17bd0140abaea70dbe6328cee9fa595e3a2
MRaflyH <raflyhendrakusuma@gmail.com>
2023-11-24 20:27:02 +0700

    Update listdinamikdriver.c

--- src/ADT/listdinamikdriver.c
+++ src/ADT/listdinamikdriver.c
@@ -3,18 +3,26 @@
 int main() {
     Word w;
     ListDinamik l;
+    Detail d;
 
     l = CreateLD();
+    printf("IsEmptyLD : %d\n", IsEmptyLD(l));
+    
     CreateWord(4, "bana", &w);
 
     for (int i = 0; i < 16; i++) {
         InsertLD(&l, w, i);
     }
 
+    printf("IsIdxValidLD 13 : %d\n", IsIdxValidLD(l, 13));
+    printf("GetLD 13 : ");    
+    DisplayWordNewLine(GetLD(l, 13));
+    printf("IsIdxValidLD 20 : %d\n", IsIdxValidLD(l, 20));
+
     DisplayLD(l);
     printf("%d\n\n", l.Capacity);
 
-    CreateWord(4, "sasa", &w);
+    CreateWord(4, "nana", &w);
     InsertLD(&l, w, 4);
 
     DisplayLD(l);
@@ -27,5 +35,13 @@ int main() {
     DisplayLD(l);
     printf("%d\n\n", l.Capacity);
 
+    CreateWord(4, "popo", &w);
+    CreateD(&d, w, w, w);
+    InsertSB(&l.Content[1], d, 0);
+    InsertSB(&l.Content[1], d, 1);
+    InsertSB(&l.Content[1], d, 2);
+
+    DisplaySemuaLD(l);
+    
     return 0;
 }
