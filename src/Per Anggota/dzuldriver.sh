9e8a0f6531c00f22abff071470991397d80b9b3c
MRaflyH <raflyhendrakusuma@gmail.com>
2023-11-24 19:13:39 +0700

    hapus

--- src/Per Anggota/dzuldriver.sh
+++
@@ -1,3 +0,0 @@
-# jangan lupa di list gcc ini tambah punya kamu "dzul.c" "dzuldriver.c"
-gcc "ADT/kata.c" "ADT/detaillagu.c" "ADT/mesinkarakter.c" "ADT/mesininput.c" "ADT/mesinfile.c" "ADT/listdinamik.c" "ADT/liststatik.c" "ADT/map.c" "ADT/queue.c" "ADT/set.c" "ADT/stack.c" "ADT/strukturberkait.c" "data.c" "rafly.c" "dzul.c" "dzuldriver.c" -o main
-./main
